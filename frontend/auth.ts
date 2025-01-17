79b1848f0ec86010fbeffed6bafadae012bf0e80
Magnus Dalkvist <magndut@gmail.com>
2025-01-17 13:10:58 +0000

    small fix

--- frontend/auth.ts
+++ frontend/auth.ts
@@ -29,7 +29,7 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     },
     authorized: async ({ auth }) => {
       // Logged in users are authenticated, otherwise redirect to login page
-      return !!auth
+      return !!auth;
     },
   },
 });
--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,7 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["cdn.discordapp.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.discordapp.com",
+      }
+    ]
   },
 };
 
