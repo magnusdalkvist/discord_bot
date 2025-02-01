767c33862590298ee161118fcc448c64ff2fc3a5
Magnus Dalkvist <magndut@gmail.com>
2025-02-01 03:22:16 +0000

    feat: update SessionProvider to use basePath and remove redundant instances in AppSidebar

--- frontend/components/app-sidebar.tsx
+++ frontend/components/app-sidebar.tsx
@@ -13,7 +13,6 @@ import {
   SidebarRail,
 } from "@/components/ui/sidebar";
 
-import { SessionProvider } from "next-auth/react";
 import { TeamSwitcher } from "./team-switcher";
 import { User } from "next-auth";
 import { DiscordLogoIcon } from "@radix-ui/react-icons";
@@ -67,19 +66,17 @@ export function AppSidebar({
   user: User | undefined;
 }) {
   return (
-    <SessionProvider>
-      <Sidebar collapsible="icon" {...props}>
-        <SidebarHeader>
-          <TeamSwitcher />
-        </SidebarHeader>
-        <SidebarContent>
-          <NavMain items={data.navMain} />
-        </SidebarContent>
-        <SidebarFooter>
-          <NavUser user={user} />
-        </SidebarFooter>
-        <SidebarRail />
-      </Sidebar>
-    </SessionProvider>
+    <Sidebar collapsible="icon" {...props}>
+      <SidebarHeader>
+        <TeamSwitcher />
+      </SidebarHeader>
+      <SidebarContent>
+        <NavMain items={data.navMain} />
+      </SidebarContent>
+      <SidebarFooter>
+        <NavUser user={user} />
+      </SidebarFooter>
+      <SidebarRail />
+    </Sidebar>
   );
 }
